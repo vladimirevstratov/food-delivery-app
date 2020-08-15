6c8d0485155c33ae9d9b5c1a6ec1dcf3b9361e65
Vladimir <vladimir.evstratov@list.ru>
2020-08-15 16:40:16 +0300

    First install

--- App/Constants/Types.js
+++
@@ -1,9 +0,0 @@
-// A list of all actions in the system.
-import { createTypes } from 'reduxsauce'
-
-export default createTypes(`
-  CART_UPDATE
-  CART_ADD
-  CART_CLEAR
-  SETTINGS_ADD
-`)
--- ios/Pods/Target Support Files/Pods-burgerroomTests/Pods-burgerroomTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_burgerroomTests : NSObject
-@end
-@implementation PodsDummy_Pods_burgerroomTests
-@end
