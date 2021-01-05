469445a17ea524e2aea20ef5e3c911cc38ede86f
Vladimir <vladimir.evstratov@list.ru>
2021-01-05 17:34:52 +0700

    App rename to FoodHunter

--- android/app/src/main/java/com/foodhunter/MainActivity.java
+++ android/app/src/main/java/com/foodhunter/MainActivity.java
@@ -1,4 +1,4 @@
-package com.veganfooddelivery;
+package com.foodhunter;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "VeganFoodDelivery";
+    return "FoodHunter";
   }
 }
