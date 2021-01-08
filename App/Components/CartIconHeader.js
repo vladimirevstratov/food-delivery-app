9059113d638327e0ab4f9e8d54b3c67d8a371b92
Vladimir <vladimir.evstratov@list.ru>
2021-01-09 02:23:41 +0700

    FD-4 Cart components to functional

--- App/Components/CartIconHeader.js
+++ App/Components/CartIconHeader.js
@@ -3,52 +3,48 @@ import {Platform} from 'react-native';
 import {Text, Button, Badge, Icon} from 'native-base';
 import {Colors} from '../Themes/';
 
-export default class CartIconHeader extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const CartIconHeader = (props) => {
+  let itemsQty = 0;
+  let itemslimitQty = '99+';
+  (props.items || []).map((section, i) => {
+    itemsQty += parseFloat(section.qty);
+  });
 
-  render() {
-    let itemsQty = 0;
-    let itemslimitQty = '99+';
-    (this.props.items || []).map((section, i) => {
-      itemsQty += parseFloat(section.qty);
-    });
+  return itemsQty === 0 ? (
+    <Button transparent onPress={props.onCartPress} style={{right: 5}}>
+      <Icon name="cart" style={{color: '#34909F', fontSize: 30}} />
+    </Button>
+  ) : (
+    <Button
+      transparent
+      onPress={props.onCartPress}
+      style={{width: 60, right: 5}}
+      badge>
+      <Badge
+        style={{
+          backgroundColor: Colors.pink0,
+          position: 'absolute',
+          right: 0,
+          top: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          borderRadius: 100,
+          height: 25,
+          zIndex: 2,
+        }}>
+        <Text>{itemsQty <= 99 ? itemsQty : itemslimitQty}</Text>
+      </Badge>
+      <Icon
+        name="cart"
+        style={{
+          color: '#34909F',
+          fontSize: 30,
+          paddingTop: Platform.OS === 'ios' ? 2 : 0,
+          paddingLeft: Platform.OS === 'ios' ? 3.4 : 1.7,
+        }}
+      />
+    </Button>
+  );
+};
 
-    return itemsQty === 0 ? (
-      <Button transparent onPress={this.props.onCartPress} style={{right: 5}}>
-        <Icon name="cart" style={{color: '#34909F', fontSize: 30}} />
-      </Button>
-    ) : (
-      <Button
-        transparent
-        onPress={this.props.onCartPress}
-        style={{width: 60, right: 5}}
-        badge>
-        <Badge
-          style={{
-            backgroundColor: Colors.pink0,
-            position: 'absolute',
-            right: 0,
-            top: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            borderRadius: 100,
-            height: 25,
-            zIndex: 2,
-          }}>
-          <Text>{itemsQty <= 99 ? itemsQty : itemslimitQty}</Text>
-        </Badge>
-        <Icon
-          name="cart"
-          style={{
-            color: '#34909F',
-            fontSize: 30,
-            paddingTop: Platform.OS === 'ios' ? 2 : 0,
-            paddingLeft: Platform.OS === 'ios' ? 3.4 : 1.7,
-          }}
-        />
-      </Button>
-    );
-  }
-}
+export default CartIconHeader;
--- App/Components/CartTotal.js
+++ App/Components/CartTotal.js
@@ -3,24 +3,21 @@ import {View} from 'react-native';
 import {Text} from 'native-base';
 import styles from '../Containers/Styles/CartScreenStyles';
 
-export default class SwipeRowCart extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <View style={styles.checkoutsummary}>
-        <View style={styles.summarycol1}>
-          <Text style={styles.summarycol1text0}>Итого</Text>
-          <Text style={styles.summarycol1text1}>
-            Скидка {this.props.skidka}% ({this.props.tax}₽)
-          </Text>
-        </View>
-        <View style={styles.summarycol2}>
-          <Text style={styles.summarycol2text0}>{this.props.total} ₽</Text>
-          <Text style={styles.summarycol2text1}>{this.props.subTotal} ₽</Text>
-        </View>
+const CartTotal = (props) => {
+  return (
+    <View style={styles.checkoutsummary}>
+      <View style={styles.summarycol1}>
+        <Text style={styles.summarycol1text0}>Итого</Text>
+        <Text style={styles.summarycol1text1}>
+          Скидка {props.skidka}% ({props.tax}₽)
+        </Text>
       </View>
-    );
-  }
-}
+      <View style={styles.summarycol2}>
+        <Text style={styles.summarycol2text0}>{props.total} ₽</Text>
+        <Text style={styles.summarycol2text1}>{props.subTotal} ₽</Text>
+      </View>
+    </View>
+  );
+};
+
+export default CartTotal;
