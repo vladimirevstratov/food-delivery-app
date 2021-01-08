670137bc8c1bf61f65ec961f9ff281e366cbd93a
Vladimir <vladimir.evstratov@list.ru>
2021-01-09 03:04:10 +0700

    FD-4 Category screen to functional

--- App/Containers/CategoryScreen.js
+++ App/Containers/CategoryScreen.js
@@ -1,6 +1,6 @@
-import {connect} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Actions from '../Actions/Creators';
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   FlatList,
@@ -27,53 +27,20 @@ import styles from './Styles/CategoryScreenStyles';
 import {Colors} from '../Themes/';
 import firebase from '@react-native-firebase/database';
 
-class CategoryScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.menuref = firebase().ref('Меню');
-    this.ref = this.menuref.orderByChild('id');
-    this.setref = firebase().ref('Настройки/Общие');
-    this.state = {
-      loading: true,
-      items: [],
-    };
-  }
+const CategoryScreen = (props) => {
+  const menuref = firebase().ref('Меню');
+  const ref = menuref.orderByChild('id');
+  const setref = firebase().ref('Настройки/Общие');
+  const [categoryState, setCategoryState] = useState({
+    loading: true,
+    items: [],
+  });
 
-  componentDidMount() {
-    console.log('>>this.props', this.props);
+  const {items} = useSelector((state) => state.cart);
 
-    BackHandler.addEventListener('hardwareBackPress', () => {
-      this.props.navigation.goBack();
-      return false;
-    });
+  const dispatch = useDispatch();
 
-    this.isConnectionOnline()
-      .then(() => {
-        return new Promise((resolve, reject) => {
-          this.setref.once('value').then((value) => {
-            resolve(value);
-          });
-        });
-      })
-      .then(this.getTax)
-      .then(() => {
-        return new Promise((resolve, reject) => {
-          this.ref.once('value').then((value) => {
-            resolve(value);
-          });
-        });
-      })
-      .then(this.getItems)
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  componentWillMount() {}
-
-  componentWillUnmount() {}
-
-  isConnectionOnline = () => {
+  const isConnectionOnline = () => {
     //Проверка на доступность интернет соединения
     return new Promise((resolve, reject) => {
       fetch('https://google.com')
@@ -94,24 +61,20 @@ class CategoryScreen extends Component {
           Toast.show({
             text: 'Нет соединения',
             position: 'bottom',
-            //buttonText: 'Обновить',
             duration: 5000,
-            //onClose: () => {this.gettingDatafromFirebase()}
           });
-          //console.log(error);
           resolve();
-          //reject("no internet connection");
         });
     });
   };
 
-  getItems = (querySnapshot) => {
-    const items = [];
-    //console.log(querySnapshot);
+  const getItems = (querySnapshot) => {
+    const newItems = [];
+
     querySnapshot.forEach((doc) => {
       const item = doc.val();
 
-      items.push({
+      newItems.push({
         name: item.название,
         measure: item.измерение,
         price: item.цена,
@@ -122,18 +85,19 @@ class CategoryScreen extends Component {
       });
     });
 
-    this.setState({
-      items,
+    setCategoryState({
+      items: newItems,
       loading: false,
     });
   };
 
-  getTax = (querySnapshot) => {
+  const getTax = (querySnapshot) => {
     const setingsapp = querySnapshot.val();
-    this.props.addSettings(setingsapp.скидка);
+
+    dispatch(Actions.addSettings(setingsapp.скидка));
   };
 
-  renderSeparator = () => {
+  const renderSeparator = () => {
     return (
       <View
         style={{
@@ -146,8 +110,8 @@ class CategoryScreen extends Component {
     );
   };
 
-  renderHeader = () => {
-    if (!this.state.loading) {
+  const renderHeader = () => {
+    if (!categoryState.loading) {
       return null;
     }
     return (
@@ -157,7 +121,7 @@ class CategoryScreen extends Component {
     );
   };
 
-  prepareToAddToCart(item) {
+  const prepareToAddToCart = (item) => {
     let newItem = {
       qty: 1,
       id: item.id,
@@ -167,102 +131,108 @@ class CategoryScreen extends Component {
       price: item.price,
     };
 
-    this.props.addCart(newItem);
-  }
-
-  handlePressItem = (item) => {
-    console.log('>>>item', item);
-    this.props.navigation.navigate('ItemScreen', {data1: item});
+    dispatch(Actions.addCart(newItem));
   };
 
-  render() {
-    return (
-      <Container style={styles.container}>
-        <Header
-          style={{backgroundColor: '#ffffff'}}
-          androidStatusBarColor={Colors.black}>
-          <Left>
-            <Button
-              transparent
-              onPress={() => this.props.navigation.navigate('DrawerOpen')}>
-              <Icon name="menu" style={{color: '#34909F'}} />
-            </Button>
-          </Left>
-          <Body style={{flex: 3}}>
-            <Title style={{color: '#222222'}}>МЕНЮ</Title>
-          </Body>
-          <Right>
-            <CartIconHeader
-              items={this.props.items}
-              onCartPress={() => this.props.navigation.navigate('CartScreen')}
-            />
-          </Right>
-        </Header>
-        <FlatList
-          data={this.state.items}
-          renderItem={({item}) => (
-            <ListItem>
-              <View style={styles.listitem0}>
-                <TouchableWithoutFeedback
-                  onPress={() => this.handlePressItem(item)}>
-                  <View style={styles.listitem0left}>
-                    <Thumbnail circle size={51} source={{uri: item.photo}} />
-                    <View style={styles.itemtext}>
-                      <Text style={styles.itemname} uppercase={true}>
-                        {item.name}
-                      </Text>
-                      <Text style={styles.itemweight}>
-                        {item.weight}
-                        {item.measure}
-                      </Text>
-                    </View>
+  const handlePressItem = (item) => {
+    props.navigation.navigate('ItemScreen', {data1: item});
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      props.navigation.goBack();
+      return false;
+    });
+
+    isConnectionOnline()
+      .then(() => {
+        return new Promise((resolve) => {
+          setref.once('value').then((value) => {
+            resolve(value);
+          });
+        });
+      })
+      .then(getTax)
+      .then(() => {
+        return new Promise((resolve, reject) => {
+          ref.once('value').then((value) => {
+            resolve(value);
+          });
+        });
+      })
+      .then(getItems)
+      .catch((err) => {
+        console.log(err);
+      });
+    /* eslint-disable-next-line */
+  }, [])
+
+  return (
+    <Container style={styles.container}>
+      <Header
+        style={{backgroundColor: '#ffffff'}}
+        androidStatusBarColor={Colors.black}>
+        <Left>
+          <Button
+            transparent
+            onPress={() => props.navigation.navigate('DrawerOpen')}>
+            <Icon name="menu" style={{color: '#34909F'}} />
+          </Button>
+        </Left>
+        <Body style={{flex: 3}}>
+          <Title style={{color: '#222222'}}>МЕНЮ</Title>
+        </Body>
+        <Right>
+          <CartIconHeader
+            items={items}
+            onCartPress={() => props.navigation.navigate('CartScreen')}
+          />
+        </Right>
+      </Header>
+      <FlatList
+        data={categoryState.items}
+        renderItem={({item}) => (
+          <ListItem>
+            <View style={styles.listitem0}>
+              <TouchableWithoutFeedback onPress={() => handlePressItem(item)}>
+                <View style={styles.listitem0left}>
+                  <Thumbnail circle size={51} source={{uri: item.photo}} />
+                  <View style={styles.itemtext}>
+                    <Text style={styles.itemname} uppercase={true}>
+                      {item.name}
+                    </Text>
+                    <Text style={styles.itemweight}>
+                      {item.weight}
+                      {item.measure}
+                    </Text>
+                  </View>
+                </View>
+              </TouchableWithoutFeedback>
+              <View style={styles.listitem0right}>
+                <TouchableWithoutFeedback onPress={() => handlePressItem(item)}>
+                  <View style={styles.itemprice}>
+                    <Text style={styles.pricetext}>{item.price}</Text>
+                    <Text style={styles.currencytext}>руб</Text>
                   </View>
                 </TouchableWithoutFeedback>
-                <View style={styles.listitem0right}>
-                  <TouchableWithoutFeedback
-                    onPress={() => this.handlePressItem(item)}>
-                    <View style={styles.itemprice}>
-                      <Text style={styles.pricetext}>{item.price}</Text>
-                      <Text style={styles.currencytext}>руб</Text>
-                    </View>
-                  </TouchableWithoutFeedback>
-                  <Button
-                    transparent
-                    style={styles.carticonbtn}
-                    onPress={() => {
-                      this.prepareToAddToCart(item);
-                    }}>
-                    <Icon name="cart" style={styles.carticon} />
-                  </Button>
-                </View>
+                <Button
+                  transparent
+                  style={styles.carticonbtn}
+                  onPress={() => {
+                    prepareToAddToCart(item);
+                  }}>
+                  <Icon name="cart" style={styles.carticon} />
+                </Button>
               </View>
-            </ListItem>
-          )}
-          ItemSeparatorComponent={this.renderSeparator}
-          ListHeaderComponent={this.renderHeader}
-          //ListFooterComponent={this.renderFooter}
-          keyExtractor={(item, index) => index.toString()}
-          //onRefresh={this.handleRefresh}
-          //refreshing={this.state.refreshing}
-          //onEndReached={this.handleLoadMore}
-          //onEndReachedThreshold={50}
-        />
-      </Container>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    items: state.cart.items,
-  };
+            </View>
+          </ListItem>
+        )}
+        ItemSeparatorComponent={renderSeparator}
+        ListHeaderComponent={renderHeader}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </Container>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addCart: (item) => dispatch(Actions.addCart(item)),
-    addSettings: (item) => dispatch(Actions.addSettings(item)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CategoryScreen);
+export default CategoryScreen;
