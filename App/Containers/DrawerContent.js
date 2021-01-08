4e425e7efea919f00bd7d0efb7b3f07dd4497e07
Vladimir <vladimir.evstratov@list.ru>
2021-01-09 02:46:14 +0700

    FD-4 Drawer content to functional

--- App/Containers/DrawerContent.js
+++ App/Containers/DrawerContent.js
@@ -1,12 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Image, Linking} from 'react-native';
 import {List, ListItem, Text, View, Content, Icon, Footer} from 'native-base';
 
 import styles from './Styles/DrawerContentStyles';
 import {Images, Colors} from '../Themes';
 
-class DrawerContent extends Component {
-  callNumber = (url) => {
+const DrawerContent = (props) => {
+  const navigation = props.navigation;
+  const items = props.items;
+
+  const callNumber = (url) => {
     Linking.canOpenURL(url)
       .then((supported) => {
         if (!supported) {
@@ -18,35 +21,32 @@ class DrawerContent extends Component {
       .catch((err) => console.error('An error occurred', err));
   };
 
-  render() {
-    const navigation = this.props.navigation;
-    const items = this.props.items;
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Image source={Images.logo} style={styles.logo} />
-        </View>
-        <Content style={styles.content}>
-          <List
-            dataArray={items}
-            renderRow={(item) => (
-              <ListItem onPress={() => navigation.navigate(item.routeName)}>
-                <Text style={{paddingLeft: 10}}>{item.routeName}</Text>
-              </ListItem>
-            )}
-          />
-        </Content>
-        <Footer style={styles.callus}>
-          <Icon name="call" style={{fontSize: 25, color: Colors.pink0}} />
-          <Text
-            style={{paddingLeft: 15}}
-            onPress={() => this.callNumber('tel:+79619974245')}>
-            Позвоните нам
-          </Text>
-        </Footer>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={Images.logo} style={styles.logo} />
       </View>
-    );
-  }
-}
+      <Content style={styles.content}>
+        <List
+          dataArray={items}
+          renderRow={(item) => (
+            <ListItem onPress={() => navigation.navigate(item.routeName)}>
+              <Text style={{paddingLeft: 10}}>{item.routeName}</Text>
+            </ListItem>
+          )}
+        />
+      </Content>
+      <Footer style={styles.callus}>
+        <Icon name="call" style={{fontSize: 25, color: Colors.pink0}} />
+        <Text
+          style={{paddingLeft: 15}}
+          //TODO move number to firebase
+          onPress={() => callNumber('tel:+79619974245')}>
+          Позвоните нам
+        </Text>
+      </Footer>
+    </View>
+  );
+};
 
 export default DrawerContent;
