f886909c158fcfd11e0ec69bf100b06d8cc7617c
Vladimir <vladimir.evstratov@list.ru>
2021-01-09 02:32:07 +0700

    FD-4 Root components to functional

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,46 +1,32 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {Platform} from 'react-native';
 import ReduxNavigation from '../Navigation/ReduxNavigation';
-import {connect} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import StartupActions from '../Redux/StartupRedux';
 import ReduxPersist from '../Config/ReduxPersist';
 import SplashScreen from 'react-native-splash-screen';
 import {Root} from 'native-base';
 import styles from './Styles/RootContainerStyles';
 
-class RootContainer extends Component {
-  constructor(props) {
-    super(props);
+const RootContainer = () => {
+  const dispatch = useDispatch();
 
-    this.state = {
-      token: '',
-      tokenCopyFeedback: '',
-    };
-  }
-
-  async componentDidMount() {
+  useEffect(() => {
     // if redux persist is not active fire startup action
     if (!ReduxPersist.active) {
-      this.props.startup();
+      dispatch(StartupActions.startup());
     }
     if (Platform.OS === 'android') {
       SplashScreen.hide();
     }
-    //registerAppListener(this.props.navigation);
-  }
+    /* eslint-disable-next-line */
+  }, []);
 
-  render() {
-    return (
-      <Root style={styles.applicationView}>
-        <ReduxNavigation />
-      </Root>
-    );
-  }
-}
+  return (
+    <Root style={styles.applicationView}>
+      <ReduxNavigation />
+    </Root>
+  );
+};
 
-// wraps dispatch to create nicer functions to call within our component
-const mapDispatchToProps = (dispatch) => ({
-  startup: () => dispatch(StartupActions.startup()),
-});
-
-export default connect(null, mapDispatchToProps)(RootContainer);
+export default RootContainer;
