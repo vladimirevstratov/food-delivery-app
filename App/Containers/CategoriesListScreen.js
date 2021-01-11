85e0e4ae3619889974986397a4a8856d55683a75
Vladimir <vladimir.evstratov@list.ru>
2021-01-11 20:47:11 +0700

    FD-4 Rest screens to functional

--- App/Containers/CategoriesListScreen.js
+++ App/Containers/CategoriesListScreen.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import firebase from '@react-native-firebase/database';
-import {connect} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {BackHandler, FlatList, ActivityIndicator} from 'react-native';
 import {
   Text,
@@ -21,46 +21,25 @@ import CartIconHeader from '../Components/CartIconHeader';
 import styles from './Styles/CategoriesListScreenStyles';
 import {Colors} from '../Themes/';
 
-class CategoriesListScreen extends React.Component {
-  constructor() {
-    super();
-    this.menuref = firebase.firestore().collection('меню');
-    this.ref = this.menuref.orderBy('id');
+const CategoriesListScreen = (props) => {
+  const [state, setState] = useState({
+    items: [],
+    loading: true,
+  });
 
-    this.unsubscribe = null;
+  const {items} = useSelector((reduxState) => reduxState.cart);
 
-    this.state = {
-      items: [],
-      loading: true,
-    };
-  }
+  const menuref = firebase.firestore().collection('меню');
+  const ref = menuref.orderBy('id');
 
-  componentDidMount() {
-    BackHandler.addEventListener('hardwareBackPress', () => {
-      this.props.navigation.goBack();
-      return true;
-    });
-    this.unsubscribe = this.ref.onSnapshot(this.listenForItems);
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  listenForItems = (querySnapshot) => {
-    const items = [];
+  const listenForItems = (querySnapshot) => {
+    const newItems = [];
 
     querySnapshot.forEach((doc) => {
       const item = doc.data();
-      const itemref = this.menuref.doc(doc.id).collection('allitems');
-      /*const subitems = this.menuref.doc(doc.id).collection('allitems').get()
-				     .then(snapshot => {
-					     snapshot.forEach(subCollectionDoc => {
-										console.log(subCollectionDoc.data());
-							 });
-						 });
- 				*/
-      items.push({
+      const itemref = menuref.doc(doc.id).collection('allitems');
+
+      newItems.push({
         key: doc.id,
         doc,
         name: item.name,
@@ -68,15 +47,14 @@ class CategoriesListScreen extends React.Component {
       });
     });
 
-    //console.log({items});
-    this.setState({
-      items,
+    setState({
+      items: newItems,
       loading: false,
     });
   };
 
-  renderHeader = () => {
-    if (!this.state.loading) {
+  const renderHeader = () => {
+    if (!state.loading) {
       return null;
     }
 
@@ -90,81 +68,80 @@ class CategoriesListScreen extends React.Component {
     );
   };
 
-  handlePressCategory = (item) => {
-    this.props.navigation.navigate('CategoryScreen', {
+  const handlePressCategory = (item) => {
+    props.navigation.navigate('CategoryScreen', {
       name: item.name,
       ref: item.itemref,
     });
   };
 
-  render() {
-    /*const counttext = 0;
-		countItemsArray = () => {
-			this.props.items.forEach((elem) => {
-				counttext+=1;
-			});
-		}*/
+  const unsubscribe = () => ref.onSnapshot(listenForItems);
 
-    //console.log(this.props.items);
-    return (
-      <Container>
-        <Header
-          style={{backgroundColor: '#ffffff'}}
-          androidStatusBarColor={Colors.black}>
-          <Left>
-            <Button
-              transparent
-              onPress={() => this.props.navigation.navigate('DrawerOpen')}>
-              <Icon name="ios-menu" style={{color: '#34909F'}} />
-            </Button>
-          </Left>
-          <Body style={{flex: 3}}>
-            <Title style={{color: '#222222'}}>МЕНЮ</Title>
-          </Body>
-          <Right>
-            <CartIconHeader
-              items={this.props.items}
-              onCartPress={() => this.props.navigation.navigate('CartScreen')}
-            />
-          </Right>
-        </Header>
-        <Content /*padder*/ style={styles.content}>
-          <FlatList
-            style={styles.list}
-            data={this.state.items}
-            renderItem={({item}) => (
-              <ListItem
-                style={styles.row0}
-                onPress={() => {
-                  this.handlePressCategory(item);
-                }}>
-                <Thumbnail
-                  circle
-                  size={51}
-                  source={require('../Images/rolls.png')}
-                />
-                <Text
-                  onPress={() => {
-                    this.handlePressCategory(item);
-                  }}
-                  style={styles.row0text}
-                  uppercase={true}>
-                  {item.name}
-                </Text>
-              </ListItem>
-            )}
-            ListHeaderComponent={this.renderHeader}
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      props.navigation.goBack();
+      return true;
+    });
+
+    return () => {
+      unsubscribe();
+    };
+
+    /* eslint-disable-next-line */
+  }, []);
+
+  return (
+    <Container>
+      <Header
+        style={{backgroundColor: '#ffffff'}}
+        androidStatusBarColor={Colors.black}>
+        <Left>
+          <Button
+            transparent
+            onPress={() => props.navigation.navigate('DrawerOpen')}>
+            <Icon name="ios-menu" style={{color: '#34909F'}} />
+          </Button>
+        </Left>
+        <Body style={{flex: 3}}>
+          <Title style={{color: '#222222'}}>МЕНЮ</Title>
+        </Body>
+        <Right>
+          <CartIconHeader
+            items={items}
+            onCartPress={() => props.navigation.navigate('CartScreen')}
           />
-        </Content>
-      </Container>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    items: state.cart.items,
-  };
+        </Right>
+      </Header>
+      <Content /*padder*/ style={styles.content}>
+        <FlatList
+          style={styles.list}
+          data={state.items}
+          renderItem={({item}) => (
+            <ListItem
+              style={styles.row0}
+              onPress={() => {
+                handlePressCategory(item);
+              }}>
+              <Thumbnail
+                circle
+                size={51}
+                source={require('../Images/rolls.png')}
+              />
+              <Text
+                onPress={() => {
+                  handlePressCategory(item);
+                }}
+                style={styles.row0text}
+                uppercase={true}>
+                {item.name}
+              </Text>
+            </ListItem>
+          )}
+          ListHeaderComponent={renderHeader}
+        />
+      </Content>
+    </Container>
+  );
 };
 
-export default connect(mapStateToProps)(CategoriesListScreen);
+export default CategoriesListScreen;
